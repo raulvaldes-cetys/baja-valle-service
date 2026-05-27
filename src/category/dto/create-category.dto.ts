64dda33b0b22fde3acbff617556058c26c9c3e27
Angela <arivas@cetys.edu.mx>
2026-05-26 19:06:02 -0700

    feat/crear-modulo-category

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,5 +1,15 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+} from 'class-validator';
 
 export class CreateCategoryDto {
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsOptional() //pendiente metodo de validacion
+    @IsString() 
     color?: string;
 }
