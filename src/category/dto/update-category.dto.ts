b0e3001a98223e51db4b2cbee4198cd377278927
Angela <arivas@cetys.edu.mx>
2026-05-26 19:09:05 -0700

    feat/crear-modulo-category

---
+++ src/category/dto/update-category.dto.ts
@@ -0,0 +1,14 @@
+import {
+    IsOptional,
+    IsString,
+} from 'class-validator';
+
+export class UpdateCategoryDto {
+    @IsOptional()
+    @IsString()
+    name?: string;
+
+    @IsOptional()
+    @IsString()
+    color?: string;
+}
